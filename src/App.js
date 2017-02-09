e30ae4cce487c5386efafbd48e4a3dd7cb8c2c54
meganetaaan <ishikawa.s.1027@gmail.com>
2017-02-10 00:17:04 +0900

    added mail form

--- src/App.js
+++ src/App.js
@@ -2,12 +2,71 @@ import React, { Component } from 'react'
 import './App.css'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import AppBar from 'material-ui/AppBar'
-import IconButton from 'material-ui/IconButton';
-import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import IconButton from 'material-ui/IconButton'
+import NavigationClose from 'material-ui/svg-icons/navigation/close'
 import Drawer from 'material-ui/Drawer'
 import MenuItem from 'material-ui/MenuItem'
+import TextField from 'material-ui/TextField'
+import Paper from 'material-ui/Paper'
+import FontIcon from 'material-ui/FontIcon'
+import RaisedButton from 'material-ui/RaisedButton'
+
 import injectTapEventPlugin from 'react-tap-event-plugin'
 injectTapEventPlugin()
+
+class Form extends Component {
+  toMailLink(args){
+    let to = ''
+    let cc = ''
+    let bcc = ''
+    if (args.addr != null) {
+      to = args.addr.to && args.addr.to.length > 0 ? args.addr.to.join(',') : ''
+      cc = args.addr.cc && args.addr.cc.length > 0 ? args.addr.cc.join(',') : ''
+      bcc = args.addr.bcc && args.addr.bcc.length > 0 ? args.addr.bcc.join(',') : ''
+    }
+    return `mailto:${to}
+        ?cc=${cc}
+        &bcc=${bcc}
+        &subject=${args.subject}
+        &body=${args.body}`
+  }
+
+  render() {
+    const mailLink = this.toMailLink(this.props)
+    return (
+      <div>
+      <TextField
+      floatingLabelText='TO'
+      fullWidth={true}
+      /><br />
+      <TextField
+      floatingLabelText='CC'
+      fullWidth={true}
+      /><br />
+      <TextField
+      floatingLabelText='BCC'
+      fullWidth={true}
+      /><br />
+      <TextField
+      floatingLabelText='Subject'
+      fullWidth={true}
+      /><br />
+      <TextField
+      floatingLabelText='Body'
+      fullWidth={true}
+      /><br />
+      <RaisedButton
+      href={mailLink}
+      target='_blank'
+      label='send to mailer'
+      secondary={true}
+      style={{margin: 12}}
+      icon={<FontIcon className='muidocs-icon-custom-mail' />}
+      />
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   constructor() {
@@ -49,14 +108,9 @@ class App extends Component {
       <header>
       <AppBar title='form2mail' onTitleTouchTap={this.handleClick} onLeftIconButtonTouchTap={this.handleClick} />
       </header>
-      <p className="App-intro">
-      <a target={'_blank'} href={
-        `mailto:${this.state.addr.to.join(',')}
-        ?cc=${this.state.addr.cc.join(',')}
-        &bcc=${this.state.addr.cc.join(',')}
-        &subject=${this.state.subject}
-        &body=${this.state.body}`} >mailto</a>
-      </p>
+      <Paper style={{margin: '1em', padding: '1em'}} zDepth={1}>
+      <Form addr={this.state.addr} subject={this.state.subject} body={this.state.body} />
+      </Paper>
       </div>
       </MuiThemeProvider>
     )
